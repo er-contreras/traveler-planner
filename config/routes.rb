bc30a5cf6d6d92a4dba8277ec5348d5f55b3e643
Erick Contreras <eri89--@hotmail.com>
2022-08-27 00:21:36 -0500

    Second commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root to: "travel#index"
+
+  get '/search' => 'travel#search'
+end
